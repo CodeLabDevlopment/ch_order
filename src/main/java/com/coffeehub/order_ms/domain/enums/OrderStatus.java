55a99ba0843f36daa8983327b9f2a616810c16cf
lucasmoraist <luksmnt1101@gmail.com>
2025-08-29 10:39:30 -0300

    chore: add tasks to doing

--- src/main/java/com/coffeehub/order_ms/domain/enums/OrderStatus.java
+++ src/main/java/com/coffeehub/order_ms/domain/enums/OrderStatus.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 @Getter
 public enum OrderStatus {
     RECEIVED("Received"),
-    PAID("Paid"),
-    PREPARING("Preparing"),
-    READY("Ready"),
-    OUT_FOR_DELIVERY("Out for Delivery"),
-    DELIVERED("Delivered"),
-    CANCELLED("Cancelled");
+    PAID("Paid"), // TODO: Após efetuar pagamento o serviço de pagamento irá enviar uma mensagem para esse serviço notificando
+    PROCESSING("Processing"), // TODO: Após pagamento pago
+    SENT("Sent"), // TODO: Após serviço de delivery enviar para entrega
+    DELIVERED("Delivered"), // TODO: Após serviço do correio notificar que foi entregue
+    CANCELLED("Cancelled"); // TODO: Quando pedido é cancelado
 
     private final String status;
 
