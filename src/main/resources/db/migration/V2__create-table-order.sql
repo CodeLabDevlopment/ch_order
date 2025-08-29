a7174a3aabcbdb080989ccf3d61fd6286a71d538
lucasmoraist <luksmnt1101@gmail.com>
2025-08-29 10:39:01 -0300

    chore: add not null in status field

--- src/main/resources/db/migration/V2__create-table-order.sql
+++ src/main/resources/db/migration/V2__create-table-order.sql
@@ -2,7 +2,7 @@ CREATE TABLE t_ch_orders (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     customer_id UUID NOT NULL,
     total_amount DECIMAL(10,2) NOT NULL,
-    status VARCHAR(20),
+    status VARCHAR(20) NOT NULL,
     payment_id UUID,
     delivery_id UUID,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
