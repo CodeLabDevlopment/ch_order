ecfea9dc60566efae861458213ce54db1683f3ca
lucasmoraist <luksmnt1101@gmail.com>
2025-08-29 00:15:30 -0300

    feat: implement OrderController and OrderRoutes for order management

---
+++ src/main/java/com/coffeehub/order_ms/infrastructure/web/controller/OrderController.java
@@ -0,0 +1,51 @@
+package com.coffeehub.order_ms.infrastructure.web.controller;
+
+import com.coffeehub.order_ms.application.usecases.order.CreateOrderCase;
+import com.coffeehub.order_ms.application.usecases.order.GetOrderByIdCase;
+import com.coffeehub.order_ms.application.usecases.order.UpdateOrderStatusCase;
+import com.coffeehub.order_ms.domain.enums.OrderStatus;
+import com.coffeehub.order_ms.domain.model.Order;
+import com.coffeehub.order_ms.infrastructure.mapper.OrderMapper;
+import com.coffeehub.order_ms.infrastructure.web.request.OrderRequest;
+import com.coffeehub.order_ms.infrastructure.web.response.order.OrderResponse;
+import com.coffeehub.order_ms.infrastructure.web.routes.OrderRoutes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController implements OrderRoutes {
+
+    private final CreateOrderCase createOrderCase;
+    private final GetOrderByIdCase getOrderByIdCase;
+    private final UpdateOrderStatusCase updateOrderStatusCase;
+
+    @Override
+    public ResponseEntity<Void> createOrder(OrderRequest request) {
+        this.createOrderCase.execute(
+                request.customerId(),
+                request.productId(),
+                request.productName(),
+                request.price(),
+                request.quantity()
+        );
+        return ResponseEntity.status(201).build();
+    }
+
+    @Override
+    public ResponseEntity<OrderResponse> getOrderById(UUID orderId) {
+        Order order = this.getOrderByIdCase.execute(orderId);
+        OrderResponse response = OrderMapper.toResponse(order);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @Override
+    public ResponseEntity<Void> updateOrderStatus(UUID orderId, OrderStatus status) {
+        this.updateOrderStatusCase.execute(orderId, status);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/com/coffeehub/order_ms/infrastructure/web/routes/OrderRoutes.java
@@ -0,0 +1,30 @@
+package com.coffeehub.order_ms.infrastructure.web.routes;
+
+import com.coffeehub.order_ms.domain.enums.OrderStatus;
+import com.coffeehub.order_ms.infrastructure.web.request.OrderRequest;
+import com.coffeehub.order_ms.infrastructure.web.response.order.OrderResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/orders")
+public interface OrderRoutes {
+
+    @PostMapping("/create")
+    ResponseEntity<Void> createOrder(@Valid @RequestBody OrderRequest request);
+
+    @GetMapping("{orderId}")
+    ResponseEntity<OrderResponse> getOrderById(@PathVariable UUID orderId);
+
+    @PatchMapping("{orderId}")
+    ResponseEntity<Void> updateOrderStatus(@PathVariable UUID orderId, @RequestParam(name = "status") OrderStatus status);
+
+}
